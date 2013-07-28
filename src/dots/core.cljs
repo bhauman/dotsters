b29d11173455f4d5a642421b8eb76638837a28ec
bhauman <bhauman@gmail.com>
2013-07-28 09:44:49 -0400

    changing to a multiple dimensional board

--- src/dots/core.cljs
+++ src/dots/core.cljs
@@ -43,7 +43,7 @@
           (recur)))
     rc))
 
-(def board-size 10)
+(def board-size 6)
 
 (defn comp-pos [pos]
   (- (dec board-size) pos))
@@ -57,14 +57,15 @@
 (defn get-rand-colors [number]
   (map (fn [x] (rand-color)) (range number)))
 
-(defn dot-pos-to-abs-position [dot-pos]
-  [(+ 30 (* 50 (- 9 dot-pos))) 30])
+(defn dot-pos-to-abs-position [[xpos ypos]]
+  [(+ 30 (* 50 (- (dec board-size) ypos)))
+   (+ 30 (* 50 xpos))])
 
 (defn dot-pos-to-center-position [dot-pos]
   (vec (map (partial + 10) (dot-pos-to-abs-position dot-pos))))
 
-(defn dot [i color]
-  (let [[top left] (dot-pos-to-abs-position i)
+(defn dot [pos color]
+  (let [[top left] (dot-pos-to-abs-position pos)
         class (str "dot " (name color))
         style (str "top:" top "px; left: " left "px;")]
     [:div {:class class :style style}]))
@@ -75,8 +76,8 @@
    [:div.board]]
 )
 
-(defn create-dot [position color]
-  {:color color :elem (crate/html (dot position color))})
+(defn create-dot [xpos ypos color]
+  {:color color :elem (crate/html (dot [xpos ypos] color))})
 
 (defn remove-dot [{:keys [elem] :as dot}]
   (.remove ($ elem)))
@@ -93,25 +94,32 @@
 (defn render-view [state]
   (inner ($ ".container")
          (crate/html (board state)))
-  (add-dots-to-board (state :board)))
+  (mapv add-dots-to-board (state :board)))
 
 (defn dot-index [{:keys [x y]}]
-  (let [board-length 10]
-    (if (and (< 40 x 60) (< 30 (mod y 50)))
-      (let [position (- 9 (int (/ y 50)))]
-        (if (> board-length position)
-          position)))))
+  (if (and (< 30 (mod x 50)) (< 30 (mod y 50)))
+    (let [ypos (- (dec board-size) (int (/ y 50)))
+          xpos (int (/ x 50))]
+      (if (and
+           (> board-size ypos)
+           (> board-size xpos))
+        [xpos ypos]))))
 
 (defn dot-follows? [state prev-dot cur-dot]
   (let [board (state :board)
-        prev-dot-color (-> board (get prev-dot) :color)
-        cur-dot-color (-> board (get cur-dot) :color)]
+        prev-dot-color (-> board (get-in prev-dot) :color)
+        cur-dot-color (-> board (get-in cur-dot) :color)]
+    (log (prn-str [prev-dot-color cur-dot-color]))
     (or (nil? prev-dot)
         (and
          (= prev-dot-color cur-dot-color)
-         (or
-          (= cur-dot (inc prev-dot))
-          (= cur-dot (dec prev-dot)))))))
+         (let [[cx cy] cur-dot
+               [px py] prev-dot]
+           (or
+            (or (= cx (inc px))
+                (= cx (dec px)))
+            (or (= cy (inc py))
+                (= cy (dec py)))))))))
 
 (defn add-dot [{:keys [dot-chain] :as state} dot-pos]
   (if (dot-follows? state (last dot-chain) dot-pos)
@@ -144,7 +152,6 @@
   (go
    (loop [state start-state]
      (let [[msg point] (<! draw-ch)]
-       (log "drawing" (prn-str msg) (prn-str state))
        (if (< 1 (count (state :dot-chain)))
          (render-dot-chain state))
        (if (= msg :drawend)
@@ -156,40 +163,67 @@
             (add-dot state dot-pos)
             state)))))))
 
-(defn render-remove-dots [state dot-chain]
-  (let [pos-set        (set dot-chain)
-        dots-to-remove (keep-indexed #(if (pos-set %1) %2) (state :board))
-        next_board     (keep-indexed #(if (not (pos-set %1)) %2) (state :board))]
+(defn render-remove-dots-row-helper [dot-chain-set col]
+  (log (prn-str dot-chain-set))
+  (let [dots-to-remove (keep-indexed #(if (dot-chain-set %1) %2) col)
+        next_col     (keep-indexed #(if (not (dot-chain-set %1)) %2) col)]
     (doseq [dot dots-to-remove]
       (remove-dot dot))
+    (log (prn-str (count next_col)))
+    (vec next_col)))
+
+(defn render-remove-dots [state dot-chain]
+  (let [dot-chain-groups  (group-by first dot-chain)
+        next_board     (map-indexed #(render-remove-dots-row-helper
+                              (set (map last (get dot-chain-groups %1)))
+                              %2)
+                            (state :board))]
     (assoc state :board (vec next_board))))
 
-(defn add-missing-dots [{:keys [board] :as state}]
-  (if (= (count board) board-size)
-    state
+(defn add-missing-dots-helper [col-idx col]
+  (if (= (count col) board-size)
+    col
     (let [new-dots (map create-dot
+                        (repeat col-idx)
                         (repeat 13)
-                        (get-rand-colors (- board-size (count board))))]
+                        (get-rand-colors (- board-size (count col))))]
       (add-dots-to-board new-dots)
-      (assoc state :board (vec (concat (state :board) new-dots))))
-    ))
+      (vec (concat col new-dots)))))
+
+(defn add-missing-dots [{:keys [board] :as state}]
+    (assoc state :board
+           (map-indexed
+            #(add-missing-dots-helper %1 %2)
+            board)))
+
+(defn render-position-updates-helper [col-idx col]
+  (go
+   (loop [cur-col col
+          pos 0]
+     (let [dot (first cur-col)
+           xd  (rest cur-col)]
+       (if (not (nil? dot))
+         (do
+           (<! (timeout 40))
+           (update-dot dot [col-idx pos])
+           (recur xd (inc pos))))))))
+
 
 (defn render-position-updates [{:keys [board]}]
-  (go
-   (loop [[dot & xd] board
-          pos 0]
-     (<! (timeout 20))
-     (update-dot dot pos)
-     (recur xd (inc pos)))))
+  (doall
+   (map-indexed
+    #(render-position-updates-helper %1 %2)
+    board)))
 
 (defn app-loop [init-state]
   (let [draw-ch (draw-chan "body")]
     (render-view init-state)
     (go
      (loop [state init-state]
+       (log "before update" (prn-str (map count (state :board))))
        (render-position-updates state)
        (let [state (add-missing-dots state)]
-         (<! (timeout 100))
+         (<! (timeout 300))
          (render-position-updates state)
          (log "rendering" (prn-str state))
          (recur
@@ -198,5 +232,12 @@
               (render-remove-dots state dot-chain)
               state))))))))
 
-(app-loop {:board (vec (map-indexed create-dot (get-rand-colors 10)) )
+(defn create-board []
+  (vec
+   (map-indexed
+    (fn [i x] (vec (map-indexed (partial create-dot i) (get-rand-colors board-size)))) 
+    (range board-size))))
+
+(app-loop {:board (create-board)
            :dot-chain []})
+
